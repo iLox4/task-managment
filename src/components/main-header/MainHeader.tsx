c729e9b5944e61dd041dd62c49183b81589da122
iLox4 <ilyaumnovvv@gmail.com>
2024-01-05 11:54:34 +0100

    MainHeader add button

--- src/components/main-header/MainHeader.tsx
+++ src/components/main-header/MainHeader.tsx
@@ -2,14 +2,37 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   margin: 0;
-  padding: 20px 0 0 20px;
-`
+  padding: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    display: inline;
+  }
+
+  button {
+    border: 1px transparent solid;
+    border-radius: 8px;
+    cursor: pointer;
+    padding: 7px 12px;
+    background-color: #626262;
+    font-size: 17px;
+    font-weight: bold;
+  }
+
+  button:hover {
+    background-color: #737373;
+  }
+`;
 
 export default function MainHeader({ title }: { title: string }) {
   //const todayDate = new Date().toLocaleDateString();
   return (
     <StyledHeader>
       <h1>{title}</h1>
+      <button>+Add</button>
     </StyledHeader>
   );
 }
